31575c3ad11d3d38a50d72002d3b34fe943c0057
EugeneKu <metis_ast@mail.ru>
2016-08-16 17:25:36 +0600

    Create footer

--- footer.php
+++ footer.php
@@ -14,10 +14,73 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="fw-container">
+			<div class="fw-col-md-4 fw-col-sm-6 fw-col-xs-12">
+				<div class="fw-col-md-12">
+					<div class="footer-title red">
+						<h6>КОНТАКТЫ</h6>
+					</div>
+				</div>
+				<div class="fw-col-md-12">
+					<div class="footer-about">
+						<p>Далеко-далеко за словесными горами в стране, гласных и согласных живут рыбные тексты. Щеке первую все города, образ алфавит. Там если агенство возвращайся рот, свой бросил.</p>
+					</div>
+				</div>
+				<div class="fw-col-md-12">
+					<div class="footer-contacts">
+						<ul class="list-unstyled">
+							<li>
+								<p><i class="fa fa-map-marker"></i> г.Астана ул.Айляляева 17</p>
+							</li>
+							<li>
+								<p><i class="fa fa-phone"></i> 8 (7172) 00 00 00</p>
+							</li>
+							<li>
+								<p><i class="fa fa-envelope"></i> info@turan.kz</p>
+							</li>
+						</ul>
+					</div>
+				</div>
+				<div class="fw-col-md-12">
+					<div class="footer-socials">
+						<ul class="list-inline">
+							<li>
+								<a href="#"><i class="fa fa-vk"></i></a>
+							</li>
+							<li>
+								<a href="#"><i class="fa fa-facebook"></i></a>
+							</li>
+							<li>
+								<a href="#"><i class="fa fa-instagram"></i></a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div class="fw-col-md-7 col-md-offset-1 fw-col-sm-6 fw-col-xs-12">
+				<div class="fw-col-md-12">
+					<div class="footer-title green">
+						<h6>ОБРАТНАЯ СВЯЗЬ</h6>
+					</div>
+				</div>
+				<div class="fw-col-md-12">
+
+				</div>
+			</div>
+		</div>
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'astana-turan' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'astana-turan' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'astana-turan' ), 'astana-turan', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
+			<div class="fw-container">
+				<div class="fw-col-md-8 fw-col-sm-8">
+					<p>© 2016 колледж Туран, г.Астана. Все права защищены.</p>
+					<p>Разработано <a href="http://yankee.kz"><img src="<?php echo get_template_directory_uri()?>/img/logoyankee.png" alt=""></a></p>
+				</div>
+				<div class="fw-col-md-4 fw-col-sm-4 text-md-right text-sm-right text-xs-right">
+					<div class="page-up">
+						<i class="fa fa-angle-up"></i>
+					</div>
+				</div>
+			</div>
+
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
